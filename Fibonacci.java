b3f1dbc31e01f9e7c04801a1d49563d0e333d83e
kareeshma14 <110341834+kareeshma14@users.noreply.github.com>
2022-08-03 14:19:58 +0530

    Fibo

---
+++ Fibonacci.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Fibonacci {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter the number of terms");
+		int n = sc.nextInt();
+		int a=0,b=1,c;
+		System.out.println("Fibonacci series is");
+		int i=0;
+		while(i<n)
+		{
+			if(i<=1)
+				c=i;
+			else
+			{
+				c=a+b;
+				a=b;
+				b=c;
+			}
+			System.out.println(c);
+			i++;
+		}
+		
+		
+		
+
+	}
+}
