43990c9997a58f88ecb9bd42d61c1e2c5a4ae794
kareeshma14 <110341834+kareeshma14@users.noreply.github.com>
2022-08-04 12:10:56 +0530

    Add files via upload

---
+++ New1.java
@@ -0,0 +1,10 @@
+public class New1 {
+	public static void main(String[] args) {
+		int a =200; int b=100;
+		System.out.println("addition of a and b is " + (a+b));
+		System.out.println("subtraction of a and b is " + (a-b));
+		System.out.println("multiplication of a and b is " + (a*b));
+		System.out.println("division of a and b is " + (a/b));
+	}
+
+}
